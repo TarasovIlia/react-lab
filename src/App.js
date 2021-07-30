23bf5a2aa3d4729c83e6b5681e12281fe23f2ca9
Iliya Tarasav <Iliya.Tarasav@itechart-group.com>
2021-07-30 09:53:14 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div>
-     
+        <div className="b1"
+        />
+             <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>hello</h1>  
     </div>
   );
 }
