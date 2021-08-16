ebd50b657ea4304cffc61a43406659f362dad4ea
Iliya Tarasav <Iliya.Tarasav@itechart-group.com>
2021-08-16 15:59:51 +0300

    add validation alarm

--- src/components/modalWindow/modalSignUp.jsx
+++ src/components/modalWindow/modalSignUp.jsx
@@ -5,10 +5,27 @@ export default function ModalSignUp ( {onClick} ) {
     const [userEmail, setEmail] =  useState('');
     const [userPass, setPass] =  useState('');
     const [validation, setValidation] =  useState(false);
+    const [validationMail, setValidationMail] =  useState(false);
+    const [totalValidation, setTotalValidation] =  useState(false);
     const [validationPass, setValidationPass] =  useState('');
+
+    useEffect(() => {
+        if (validation && validationMail) {
+            setTotalValidation(true)
+        }
+        else {
+            setTotalValidation(false)
+        }
+    })
 
     const sendEmail = (e) => {
         setEmail(e.target.value)
+        if (e.target.value.length >=5) {
+            setValidationMail(true)
+        }
+        else {
+            setValidationMail(false)
+        }
     }
 
     const validationPassword = (e) => {
@@ -33,18 +50,26 @@ export default function ModalSignUp ( {onClick} ) {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({  email: userEmail,
-                                    password: userPass
-        })
-        };
-        fetch('http://localhost:3000/User/1', requestOptions);
-        onClick()
-        window.location.pathname = '/'
+        if (totalValidation) {
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({  email: userEmail,
+                                        password: userPass
+            })
+            };
+            fetch('http://localhost:3000/User/1', requestOptions);
+            onClick()
+            window.location.pathname = '/'
+        }
     }
 
+    function alarmIndicator() {
+        if (validation || !validationPass) {
+            return true
+        }
+        else return false
+    }
     return (
         <div className='wrapper-modal-window'>
             <div className='modal-window'>
@@ -56,7 +81,8 @@ export default function ModalSignUp ( {onClick} ) {
                         <input onChange={sendEmail} className='search-input' type='email' placeholder='your e-mail' />
                         <input onChange={sendPassword} className='search-input' type='password' placeholder='your password' />
                         <input onChange={validationPassword} className='search-input' type='password' placeholder='repead password' />
-                        <button style={{ opacity : validation ? '1' : '0.2' }} onClick={ handleSubmit } className='modal-button'><p>sign in</p></button>
+                        <aside style={{bottom : alarmIndicator() ?  '-120px' : '0px'}} className='alarm'><p>Password mismatch!</p></aside>
+                        <button style={{ opacity : totalValidation ? '1' : '0.2' }} onClick={ handleSubmit } className='modal-button'><p>sign in</p></button>
                     </form>
                 </div>
             </div> 
