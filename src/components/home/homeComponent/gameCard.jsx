f838348bf423f62416179b842be83f0c01a91fc1
Iliya Tarasav <Iliya.Tarasav@itechart-group.com>
2021-08-24 13:55:49 +0300

    add platform icon

--- src/components/home/homeComponent/gameCard.jsx
+++ src/components/home/homeComponent/gameCard.jsx
@@ -1,36 +1,41 @@
 import React from 'react';
+import PC from '../../../icon/icons8-microsoft.svg'
+import XboX from '../../../icon/icons8-xbox.svg'
+import PS5 from '../../../icon/icons8-playstation.svg'
 
 import PropTypes from 'prop-types';
 
 import Star from './elemenst/star'
 
-class GameCard extends React.Component {
-    render () {
-        let starCounter = this.props.gameData.stars
+export default function GameCard (props) {
+        let starCounter = props.gameData.stars
         let stars = [...Array(parseInt(starCounter))].map((i) => <Star key={i}/>)
         
         return (
             <div className='game-card-wrapper'>
                  <div className='game-card front'>
-                    <div><img src={this.props.gameData.imgGame} alt="" /></div>
+                    <div className='icon-container'>
+                        { props.gameData.platform.PC ? <img className="platform-icon" src={PC} alt="" /> : null }
+                        { props.gameData.platform.XboxOne ? <img className="platform-icon" src={XboX} alt="" /> : null }
+                        { props.gameData.platform.Playstation5 ? <img className="platform-icon" src={PS5} alt="" /> : null }
+                    </div>
+                    <div><img src={props.gameData.imgGame} alt="" /></div>
                     <div className='game-price-and-name'>
                         <div className='row'>
-                            <p>{this.props.gameData.gameName}</p>
-                            <p>{this.props.gameData.priceGame} {this.props.gameData.currency}</p>
+                            <p>{props.gameData.gameName}</p>
+                            <p className="price">{props.gameData.priceGame} {props.gameData.currency}</p>
                         </div>
                         <div className='star-game-rate'>{stars}</div >
                     </div>
                 </div>
                 <div className='game-card back'>
-                    <p>description: {this.props.gameData.discription}</p>
-                    <p>{this.props.gameData.age}+</p>
+                    <p>description: {props.gameData.discription}</p>
+                    <p>{props.gameData.age}+</p>
                 </div>
             </div>
         )
     }
-}
 
 GameCard.propTypes = {
     gameData: PropTypes.any,
 };
-export default GameCard
